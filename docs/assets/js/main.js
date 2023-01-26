464030893438fe0693a8d6b030add95f4a24f349
Vladimir <ryzhovvladimirr@gmail.com>
2023-01-26 09:44:30 +0200

    add auctions section

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,7 +1,20 @@
-console.log(1)
-console.log(1)
-console.log(1)
-console.log(1)
-console.log(1)
+var swiper = new Swiper(".auct-swiper", {
+  // loop: true,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  // modules: [Navigator],
+  // autoHeight: true,
+  // speed: 500,
+  // slidesPerView: 3,
+
+  slidesPerView: 2.8,
+  spaceBetween: 42,
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+});
 console.log(2)
 console.log(3)
